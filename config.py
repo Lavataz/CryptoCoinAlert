f24a1ca1f8d30a598e0a812e95b9253deb688ef3
Lavataz <roy.aerts@aertsconnect.nl>
2026-01-10 16:08:14 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Welke coin wil je monitoren?
+COIN = "XLM-EUR"
+
+# Stuur een alert als de prijs boven deze waarde komt
+THRESHOLD = 0.095
+
+# Telegram instellingen
+TELEGRAM_TOKEN = "JOUW_TELEGRAM_BOT_TOKEN"
+CHAT_ID = "JOUW_CHAT_ID"
